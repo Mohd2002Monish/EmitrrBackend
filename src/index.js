50914de8632308de01f6a08b98754282d4d66ff2
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2023-09-29 02:20:31 +0530

    Backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
-
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
