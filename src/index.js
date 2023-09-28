43be971b39d6d043b26e5782d09414b010f906c6
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2023-09-29 02:18:32 +0530

    Backend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
+
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
 const DB = process.env.DB;
-console.log(DB, PORT);
-const authRoutes = require("./Routes/authRoutes");
 
+const authRoutes = require("./Routes/authRoutes");
+const app = express();
+app.use(cors());
 app.use("/", authRoutes);
 
 mongoose.connect(DB).then(() => {
