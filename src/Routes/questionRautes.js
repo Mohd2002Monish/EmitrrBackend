90c8790184674f8cfe169c6dd99c6ebbdb02d06c
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2023-09-30 01:38:40 +0530

    Question Routes Updated

--- src/Routes/questionRautes.js
+++ src/Routes/questionRautes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const Questions = require("../Model/questions.model");
 const app = express();
 app.get("/questions", async (req, res) => {
-  const questions = await Questions.find({});
+  const questions = await Questions.aggregate([
+    { $match: { level: "hard" } },
+    { $sample: { size: 10 } },
+  ]);
   res.send(questions);
 });
 module.exports = app;
