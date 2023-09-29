9c4980a6ee589f1d6ad4475905a3652bf8bbd2e4
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2023-09-30 01:07:35 +0530

    Question Routes Updated

--- src/Routes/questionRautes.js
+++ src/Routes/questionRautes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const Questions = require("../Model/questions.model");
 const app = express();
-app.get("/line", (req, res) => {
+app.get("/questions", (req, res) => {
   res.send({ msg: "King of the world" });
 });
 module.exports = app;
